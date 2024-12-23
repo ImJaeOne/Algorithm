3ed62fc365519a1015852406570af38463b9f3e3
Jaeone Lim <dlawi0108@naver.com>
2024-12-23 13:12:12 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 13.37 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -2,7 +2,6 @@ function solution(t, p) {
     var answer = 0;
     var t = t.split('');
     var t_ = [];
-    var n = [];
     for(i = 0; i < t.length - p.length + 1; i++){
         t_ = t.slice(i, i + p.length).join('');
         if(Number(t_) <= Number(p)){
