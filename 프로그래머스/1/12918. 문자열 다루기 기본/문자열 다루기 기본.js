63ec4e4be662559814eaddb55af0aef665948841
Jaeone Lim <dlawi0108@naver.com>
2025-01-30 04:43:24 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,14 +1,13 @@
 function solution(s) {
-    var answer = false;
-    var len = s.length;
-    s = s.split('').map(Number);
-    if(len === 4 || len === 6){
-        answer = true;
-        s.forEach((a) => {
-            if(isNaN(a)){
-                return answer = false;
-            }
-        });
+    const sArr= s.split('');
+    if(s.length !== 4 && s.length !==6){
+        return false;
     }
-    return answer;
+    for(let sItem of sArr){
+        if(isNaN(sItem)){
+            return false;
+        }
+    }
+
+    return true;
 }
