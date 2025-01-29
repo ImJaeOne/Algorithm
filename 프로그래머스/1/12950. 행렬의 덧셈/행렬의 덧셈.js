c9ce9a0a2f436bd57dc9fdf5d4a5d89426d306ad
Jaeone Lim <dlawi0108@naver.com>
2025-01-30 05:03:25 +0900

    [level 1] Title: 행렬의 덧셈, Time: 20.54 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,12 +1,9 @@
 function solution(arr1, arr2) {
-    var answer = [];
-    var a = [];
-    for(i = 0; i < arr1.length; i++){
-        for(j = 0; j < arr1[0].length; j++){
-            a.push(arr1[i][j] + arr2[i][j]);
-        }
-        answer.push(a);
-        a = [];
-    }
+    let answer = Array.from({length : arr1.length}, () => []);
+    arr1.forEach((arr, i) => {
+        arr.forEach((arrItem, j) => {
+            answer[i].push(arrItem + arr2[i][j]);
+        })
+    })
     return answer;
 }
