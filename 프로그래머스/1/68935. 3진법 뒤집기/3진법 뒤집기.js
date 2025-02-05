543236d0c8ce280f53f97028b77db059c100d6dc
Jaeone Lim <dlawi0108@naver.com>
2025-02-05 09:43:07 +0900

    [level 1] Title: 3진법 뒤집기, Time: 5.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,18 +1,25 @@
 function solution(n) {
-    var answer = 0;
-    var a = [];
-    var b = [];
-    for(i = 1; i <= n; i = i * 3){
-        a.push(i);
+    let count = 0;
+    let k = n;
+    while(k / 3 >= 1){
+        k = k / 3;
+        count++;
     }
-    a.sort((a, b) => b - a);
-    for(i = 0; i < a.length; i++){
-        b.push(Math.floor(n / a[i]));
-        n = n % a[i];
+    let answer = [];
+    for(let i = count; i >= 0; i--){
+        let item = 1;
+        for(let j = 1; j <= i; j++){
+            item *= 3;
+        }
+        answer.push(Math.floor(n / item));
+        n = n % item;
     }
-    b.reverse();
-    answer = a.reduce((acc, cur, i) => {
-        return acc +=  cur * b[i];
+    console.log(answer)
+    answer.reverse();
+    return answer.reduce((acc, cur, idx) => {
+        for(let i = answer.length - idx; i > 1; i--){
+            cur *= 3; 
+        }        
+        return acc + cur;
     }, 0)
-    return answer;
 }
--- 프로그래머스/1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/1/68935. 3진법 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.19 ms
+메모리: 33.5 MB, 시간: 5.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 17일 14:51:04
+2025년 02월 05일 09:43:05
 
 ### 문제 설명
 
