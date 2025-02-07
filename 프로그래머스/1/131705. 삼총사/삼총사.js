311d88315a49a110a85928989b3fa81792777038
Jaeone Lim <dlawi0108@naver.com>
2025-02-07 09:18:54 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,14 +1,14 @@
 function solution(number) {
-    var answer = 0;
-    for (let i = 0; i < number.length - 2; i++) {
-        for (let j = i + 1; j < number.length - 1; j++) {
-            for (let k = j + 1; k < number.length; k++) {
-                if (number[i] + number[j] + number[k] === 0) {
-                    answer += 1;
+    let answer = 0;
+    let numberLen = number.length;
+    for(let i = 0; i < numberLen - 2; i++){
+        for(let j = i + 1; j < numberLen - 1; j++){
+            for(let k = j + 1; k < numberLen; k++){
+                if(number[i] + number[j] + number[k] === 0){
+                    answer++;
                 }
             }
         }
     }
-
     return answer;
 }
