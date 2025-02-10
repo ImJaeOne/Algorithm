b8b003d9b1c80764df0aeb020b50e1c836dc74c0
Jaeone Lim <dlawi0108@naver.com>
2025-02-10 09:40:18 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 22.26 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,16 +1,14 @@
 function solution(s) {
-    var answer = [];
-    var sArr = s.split('');
-    var a = {};
-    
-    sArr.forEach((sItem, i) => {
-        if(!a[sItem]){
-            a[sItem] = [i];
+    let answer = [];
+    let newS = []; 
+    s.split('').forEach((sItem, idx) => {       
+        const findS = newS.lastIndexOf(sItem); 
+        if (findS === -1) {
             answer.push(-1);
-        }else{
-            a[sItem].push(i);  
-            answer.push(a[sItem][a[sItem].length - 1] - a[sItem][a[sItem].length - 2]);
+        } else {
+            answer.push(idx - findS);
         }
-    })
+        newS.push(sItem); 
+    });
     return answer;
 }
