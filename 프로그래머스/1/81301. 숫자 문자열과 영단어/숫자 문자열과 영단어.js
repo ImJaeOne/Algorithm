2ad23d100310b72f2c36366070406e6d8a293805
Jaeone Lim <dlawi0108@naver.com>
2025-02-14 09:28:27 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,11 +1,12 @@
 function solution(s) {
-    var answer = 0;
-    var num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    num.forEach((num, i) => {
-        while(s.includes(num)){
-            s = s.replace(num, i);
-        } 
-    })
-    answer = Number(s);
-    return answer;
+    const numArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    while(isNaN(s) === true){
+        for(let i = 0; i < numArr.length; i++){
+            if(s.includes(numArr[i])){
+                s = s.replace(numArr[i], i);
+                break;
+            }
+        }
+    }
+    return Number(s);
 }
