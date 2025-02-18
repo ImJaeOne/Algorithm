9d44faf6a5a16dec9cc0a89b0fbc96dd51443dc5
Jaeone Lim <dlawi0108@naver.com>
2025-02-18 19:30:33 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,10 +1,8 @@
 function solution(strings, n) {
-    var answer = [];
-    // 사전 순으로 먼저 배열
-    answer = strings.sort();
-    // n번째 기준 배열
-    answer = strings.sort((a, b) => {
-        return a[n].localeCompare(b[n]);
-    })
-    return answer;
+    return strings.sort((a, b) => {
+        if (a[n] === b[n]) {
+            return a > b ? 1 : -1; 
+        }
+        return a[n] > b[n] ? 1 : -1; 
+    });
 }
