7e2079460a3333614595a877a9d72531320fd30e
Jaeone Lim <dlawi0108@naver.com>
2025-03-14 09:25:06 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.43 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,24 +1,15 @@
 function solution(n, arr1, arr2) {
     let len = Math.max(...arr1, ...arr2).toString(2).length;
-    let answer = Array.from({length: len}, () => []);
+    let answer = Array.from({lenght: len}, () => []);
+    console.log(answer);
     const transA = arr1.map((a) => a.toString(2).padStart(len,0).split(''));
     const transB = arr2.map((a) => a.toString(2).padStart(len,0).split(''));
+    console.log(transA);
+    console.log(transB);
     for(let i = 0; i < len; i++){
         for(let j = 0; j < len; j++){
-            answer[i][j] = Number(transA[i][j]) + Number(transB[i][j])
+            // answer[i][j] = Number(transA[i][j]) + Number(transB[i][j])
         }
-    }
-    for(let i = 0; i < len; i++){
-        for(let j = 0; j < len; j++){
-            if(answer[i][j] === 0){
-                answer[i][j] = ' '
-            }else{
-                answer[i][j] = '#'
-            }
-        }
-    }
-    for(let i = 0; i < len; i++){
-        answer[i] = answer[i].join('')
     }
     return answer;
 }
